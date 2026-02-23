5feed0004cff37310d7cffd9f42b325a75658990
grigorykkk <gkilich5@gmail.com>
2026-02-23 13:51:02 +0300

    created git repository

---
+++ desktop/Library/Library/LibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LibraryApp.swift
+//  Library
+//
+//  Created by Григорий Костин on 11.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct LibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
